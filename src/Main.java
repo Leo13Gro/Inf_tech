1fc2d56f8bc49128051e3d469382a0bebbc733d0
Kuimov Timofey <111085197+Leo13Gro@users.noreply.github.com>
2023-10-23 21:30:52 +0300

    Task-6

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,44 @@
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
-    public static final double PI = 3.1415926535;
-    public static int n = 100000;
-    public static double d = 2 * PI / (n-1);
-    public static double[] x = new double[n+2];
-    public static double a(double x){
-        return Math.sin(x);
-//        return 2;
+    static PrintWriter printWriter;
+    public static final double dt = 1e-2;
+    public static final int n = 2000;
+    public static double f1(double x, double y){
+        return (1 - y) * x;
     }
-    public static double b(double x){
-        return Math.sin(x) + 3 * Math.sin(3 * x);
-//        return Math.sin(x);
+    public static double f2(double x, double y){
+        return (-1 + 2 * x) * y;
     }
-    public static double f_i(double f_i_2, double f_i_1, double a_i_1, double b_i_1){
-        return 2 * f_i_1 - a_i_1 * f_i_1 * d * d - f_i_2 + b_i_1 * d * d;
+    public static void writeGraph(int k) throws FileNotFoundException {
+        double[] x = new double[n];
+        double[] y = new double[n];
+        double t = 0;
+        x[0] = (4 + k) / 10.;
+        y[0] = 1;
+        for (int i = 1; i < n; i++) {
+            x[i] = x[i-1] + f1(x[i-1], y[i-1]) * dt;
+            y[i] = y[i-1] + f2(x[i-1], y[i-1]) * dt;
+            t = t + dt;
+        }
+        printWriter.print("x" + k + " = [");
+        for (int i = 0; i < n - 1; i++) {
+            printWriter.print(x[i] + ",");
+        }
+        printWriter.print(x[n-1] + "]");
+        printWriter.print("\ny" + k + " = [");
+        for (int i = 0; i < n - 1; i++) {
+            printWriter.print(y[i] + ",");
+        }
+        printWriter.print(y[n-1] + "]");
+        printWriter.println("\n");
     }
-    public static double f(double el, double[] res){
-        int n = Arrays.binarySearch(x, el);
-        if (n >= 0){
-//            System.out.println(x[n]);
-            return res[n];
+    public static void main(String[] args) throws FileNotFoundException {
+        printWriter = new PrintWriter("C:\\Users\\User\\IdeaProjects\\Inf_tech\\Graph.txt");
+        for (int i = 1; i < 11; i++) {
+            writeGraph(i);
         }
-        else {
-            int ind = - n - 1;
-//            System.out.println(x[1-n]);
-            return res[ind-1] + (res[ind] - res[ind-1]) / (x[ind] - x[ind-1]) * (el - x[ind-1]);
-        }
-    }
-    public static void main(String[] args){
-        double[] a = new double[n+2];
-        double[] b = new double[n+2];
-        for (int i = 0; i < n+2; i++){
-            x[i] = d * (i-1);
-            a[i] = a(x[i]);
-            b[i] = b(x[i]);
-        }
-        double[] f_1 = new double[n+2];
-        double[] f_2 = new double[n+2];
-        double[] f_part = new double[n+2];
-        f_1[0] = 0; f_1[1] = d;
-        f_2[0] = d; f_2[1] = 0;
-        f_part[0] = d; f_part[1] = d;
-        for (int i = 2; i < n+2; i++){
-            f_1[i] = f_i(f_1[i-2], f_1[i-1], a[i-1], 0);
-            f_2[i] = f_i(f_2[i-2], f_2[i-1], a[i-1], 0);
-            f_part[i] = f_i(f_part[i-2], f_part[i-1], a[i-1], b[i-1]);
-        }
-        double k = f_1[1] - f_1[n];
-        double l = f_2[1] - f_2[n];
-        double m = f_part[n] - f_part[1];
-        double c = f_1[2] - f_1[n+1];
-        double e = f_2[2] - f_2[n+1];
-        double f = f_part[n+1] - f_part[2];
-        double c2 = (f - c*m/k) / (e - c*l/k);
-        double c1 = (m - l*c2) / k;
-//        System.out.println("k = " + k);
-//        System.out.println("l = " + l);
-//        System.out.println("m = " + m);
-//        System.out.println("c = " + c);
-//        System.out.println("e = " + e);
-//        System.out.println("f = " + f);
-//        System.out.println("f_1[1] = " + f_1[1]);
-//        System.out.println("f_1[n] = " + f_1[n]);
-//        System.out.println("f_1[2] = " + f_1[2]);
-//        System.out.println("f_1[n+1] = " + f_1[n+1]);
-//        System.out.println("f_2[1] = " + f_2[1]);
-//        System.out.println("f_2[n] = " + f_2[n]);
-//        System.out.println("f_2[2] = " + f_2[2]);
-//        System.out.println("f_2[n+1] = " + f_2[n+1]);
-//        System.out.println(e - c*l/k);
-        double[] f_res = new double[n+2];
-        for (int i = 0; i < n + 2; i++) {
-            f_res[i] = c1 * f_1[i] + c2 * f_2[i] + f_part[i];
-        }
-//        System.out.println(f(0, f_res));
-//        System.out.println(f(PI/6, f_res) - f(0, f_res));
-//        System.out.println(f(PI/3, f_res) - f(0, f_res));
-//        System.out.println(f(PI/2, f_res) - f(0, f_res));
-//        System.out.println(f(PI, f_res) - f(0, f_res));
-//        System.out.println(f(3*PI/2, f_res) - f(0, f_res));
-//        System.out.println(f_res[1] + " " + f_res[n]);
-//        System.out.println(f_res[2] + " " + f_res[n+1]);
-        System.out.println("f(1) = " + f(1, f_res));
-        System.out.println("f(2) = " + f(2, f_res));
+        printWriter.close();
     }
 }
