a1782341f1817cafbd8e6c4a62cca7ddd9e15c82
Timofey <ya.Leo13Gro@ya.ru>
2023-10-24 14:45:33 +0300

    Task-5 without comments

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,9 @@ public class Main {
     public static double[] x = new double[n+2];
     public static double a(double x){
         return Math.sin(x);
-//        return 2;
     }
     public static double b(double x){
         return Math.sin(x) + 3 * Math.sin(3 * x);
-//        return Math.sin(x);
     }
     public static double f_i(double f_i_2, double f_i_1, double a_i_1, double b_i_1){
         return 2 * f_i_1 - a_i_1 * f_i_1 * d * d - f_i_2 + b_i_1 * d * d;
@@ -19,12 +17,10 @@ public class Main {
     public static double f(double el, double[] res){
         int n = Arrays.binarySearch(x, el);
         if (n >= 0){
-//            System.out.println(x[n]);
             return res[n];
         }
         else {
             int ind = - n - 1;
-//            System.out.println(x[1-n]);
             return res[ind-1] + (res[ind] - res[ind-1]) / (x[ind] - x[ind-1]) * (el - x[ind-1]);
         }
     }
@@ -55,33 +51,10 @@ public class Main {
         double f = f_part[n+1] - f_part[2];
         double c2 = (f - c*m/k) / (e - c*l/k);
         double c1 = (m - l*c2) / k;
-//        System.out.println("k = " + k);
-//        System.out.println("l = " + l);
-//        System.out.println("m = " + m);
-//        System.out.println("c = " + c);
-//        System.out.println("e = " + e);
-//        System.out.println("f = " + f);
-//        System.out.println("f_1[1] = " + f_1[1]);
-//        System.out.println("f_1[n] = " + f_1[n]);
-//        System.out.println("f_1[2] = " + f_1[2]);
-//        System.out.println("f_1[n+1] = " + f_1[n+1]);
-//        System.out.println("f_2[1] = " + f_2[1]);
-//        System.out.println("f_2[n] = " + f_2[n]);
-//        System.out.println("f_2[2] = " + f_2[2]);
-//        System.out.println("f_2[n+1] = " + f_2[n+1]);
-//        System.out.println(e - c*l/k);
         double[] f_res = new double[n+2];
         for (int i = 0; i < n + 2; i++) {
             f_res[i] = c1 * f_1[i] + c2 * f_2[i] + f_part[i];
         }
-//        System.out.println(f(0, f_res));
-//        System.out.println(f(PI/6, f_res) - f(0, f_res));
-//        System.out.println(f(PI/3, f_res) - f(0, f_res));
-//        System.out.println(f(PI/2, f_res) - f(0, f_res));
-//        System.out.println(f(PI, f_res) - f(0, f_res));
-//        System.out.println(f(3*PI/2, f_res) - f(0, f_res));
-//        System.out.println(f_res[1] + " " + f_res[n]);
-//        System.out.println(f_res[2] + " " + f_res[n+1]);
         System.out.println("f(1) = " + f(1, f_res));
         System.out.println("f(2) = " + f(2, f_res));
     }
