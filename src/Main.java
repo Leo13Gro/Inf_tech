f6ce95135709738c5f5126d12fa6e37e1eea43e1
Kuimov Timofey <111085197+Leo13Gro@users.noreply.github.com>
2023-11-15 11:56:21 +0300

    Task-8

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static int diff(char c1, char c2){
-        if (c1 == c2)
-            return 0;
-        return 1;
-    }
-    public static int min(int a, int b, int c){
-        int temp = Math.min(a, b);
-        return Math.min(temp, c);
-    }
-    public static void main(String[] args) {
+    public static final int e = 17;
+    public static final int n = 77;
+    public static final int d = 53;
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        char[] str_1 = sc.nextLine().toCharArray();
-        char[] str_2 = sc.nextLine().toCharArray();
-        int n = str_1.length;
-        int m = str_2.length;
-        int [][] D = new int[n+1][m+1];
-        for (int i = 0; i <= n; i++) {
-            D[i][0] = i;
+        int m = sc.nextInt();
+        int c = 1;
+        for (int i = 0; i < e; i++) {
+            c = (c * m) % n;
         }
-        for (int j = 0; j <= m; j++) {
-            D[0][j] = j;
+        System.out.println("c = " + c);
+        int res = 1;
+        for (int i = 0; i < d; i++) {
+            res = (res * c) % n;
         }
-        int t;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                t = diff(str_1[i-1], str_2[j-1]);
-                D[i][j] = min(D[i-1][j] + 1, D[i][j-1] + 1, D[i-1][j-1] + t);
-            }
-        }
-        System.out.println(D[n][m]);
+        System.out.println("m = " + res);
     }
 }
